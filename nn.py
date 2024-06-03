286b2cb9dfed3cf7fcefc8845a2f1ed957a321e9
Garv <garv1908@gmail.com>
2024-06-03 17:35:32 +0400

    implement forward propagation

--- nn.py
+++ nn.py
@@ -28,3 +28,27 @@ for epoch in range(epochs):
             # l: 10 -> (10, 1)
         img.shape += (1, )
         l.shape += (1, )
+
+        # forward propagation: transform input values to output values
+            # input -> hidden
+            # take inputs (img) and weight matrix (connects both layers),
+            # multiply through matrix multiplication (@),
+            # add bias weights
+        h_pre = b_i_h + w_i_h @ img
+
+            # normalize values into a specific range (0 <= x <= 1) by applying an activation function (the Sigmoid function)
+        h = 1 / (1 + np.exp(-h.pre))
+
+            # hidden -> output
+            # repeat procedure to get output values
+        o_pre = b_h_o + w_h_o @ h
+        o = 1 / (1 + np.exp(-o_pre))
+
+        # error/cost function (calculation)
+            # compare output with label, using mean-squared error
+        e = 1 / len(o) * np.sum((o - l) ** 2, axis=0)
+
+            # how many images are classified correctly
+        nr_correct == int(np.argmax(o) == np.argmax(l))
+
+       
