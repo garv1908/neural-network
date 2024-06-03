ce636b1aad1d7bedc77ac7e566cc2bd290fe4fea
Garv <garv1908@gmail.com>
2024-06-03 16:31:38 +0400

    finish get_mnist() function

--- data.py
+++ data.py
@@ -9,7 +9,15 @@ def get_mnist():
     images = images.astype("float32") / 255
     
     # reshape the images array to a [ 7000 x 784 ] matrix
+        # images is originally imported as a [ 7000 x 28 x 28 ] 3D matrix
         # images.shape[0]: number of images (i.e. 7000)
         # images.shape[1], images.shape[2]: height, width (i.e. 28, 28)
-        # images is originally imported as a [ 7000 x 28 x 28 ] 3D matrix
     images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
+    
+    # One-hot encoding the labels data
+        # np.eye(10) creates a 10x10 matrix with 1's on the diagonal and 0's everywhere else
+        # the 1's are populated on the indices of the labels imported earlier.
+        # this allows each label to be converted into a one-hot encoded vector
+    labels = np.eye(10)[labels]
+
+    return images, labels
