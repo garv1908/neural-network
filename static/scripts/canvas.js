c6e6b27e8dce8669767c6b03190d96c916976fd4
Garv <garv1908@gmail.com>
2024-06-11 01:29:18 +0400

    implement basic canvas using javascript

--- static/scripts/canvas.js
+++ static/scripts/canvas.js
@@ -1,3 +1,43 @@
 "use strict";
 
-const ctx = document.getElementsByTagName("canvas")
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext('2d');
+let painting = false;
+
+
+function startPosition(ev) {
+    painting = true;
+    ctx.beginPath();
+    draw(ev);
+}
+
+
+function endPosition(ev) {
+    painting = false;
+    ctx.closePath();
+}
+
+
+ctx.lineWidth = 10;
+ctx.lineCap = 'round';
+ctx.strokeStyle = 'black';
+
+
+function draw(ev) {
+    if (!painting) return;
+
+    ctx.lineTo(ev.clientX - canvas.offsetLeft, ev.clientY - canvas.offsetTop);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(ev.clientX - canvas.offsetLeft, ev.clientY - canvas.offsetTop);
+}
+
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+canvas.addEventListener("mousedown", startPosition);
+canvas.addEventListener("mouseup", endPosition);
+canvas.addEventListener("mousemove", draw);
