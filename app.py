d2eab1dfac5ba16b9452d63b7f9bf73ca7854e31
Garv <garv1908@gmail.com>
2024-06-16 21:25:47 +0400

    change variable names

--- app.py
+++ app.py
@@ -34,60 +34,62 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-def process_image(image_data):
+def processImage(imageData):
 
-    print("in process_image")
-    image_data = image_data.split(',')[1]
-    print("image_data", image_data)
-    image_bytes = base64.b64decode(image_data)
-    print("image_bytes", image_bytes)
+    print("in processImage")
+    imageData = imageData.split(',')[1]
+    print("imageData", imageData)
+    imageBytes = base64.b64decode(imageData)
+    print("imageBytes", imageBytes)
 
-    image = Image.open(io.BytesIO(image_bytes))
+    image = Image.open(io.BytesIO(imageBytes))
     print("image open")
-    image.show()
+    # image.show()
     image = image.resize((28, 28))
     print("image", image)
     image = np.array(image)
 
     print('shape:', image.shape[:])
-    print(image[:, :, 0])
-    print(image[:, :, 1])
-    print(image[:, :, 2])
-    print(image[:, :, 3])
+    # print(image[:, :, 0])
+    # print(image[:, :, 1])
+    # print(image[:, :, 2])
+    # print(image[:, :, 3])
     greyImage = image[:, :, 3]
-    plt.imshow(image, cmap="Greys")
-    plt.show()
+    # plt.imshow(image, cmap="Greys")
+    # plt.show()
     print(image)
     print(greyImage)
     print('shape:', greyImage.shape[:])
-    plt.imshow(greyImage, cmap="Greys")
-    plt.show()
+    # plt.imshow(greyImage, cmap="Greys")
+    # plt.show()
     print("np arr greyImage", greyImage)
     greyImage = greyImage.astype("float32") / 255
     print("greyImage as type normalized", greyImage)
     greyImage = np.reshape(greyImage, (greyImage.shape[0] * greyImage.shape[1]))
     print("greyImage as type normalized", greyImage)
     print('shape after:', greyImage.shape[:])
-
     return greyImage
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
+
     print('data:', data)
     if 'image' not in data:
         return jsonify({'error': 'No image data provided'}), 400
 
-    image_data = data['image']
-    print('image data:', image_data)
-    processed_data = process_image(image_data)
-    print(f"Processed data: {processed_data[:]}")
+    imageData = data['image']
+    print('image data:', imageData)
+    greyscaleImage = processImage(imageData)
+    print(f"Processed data: {greyscaleImage}")
 
-    prediction = GetPrediction(processed_data)
-    predicted_label = np.argmax(prediction)
-    print(f"Prediction: {prediction}, Predicted label: {predicted_label}")
+    prediction = GetPrediction(greyscaleImage)
+    label = np.argmax(prediction)
+
+    print(prediction)
+    print(label)
     
-    return jsonify({'prediction': int(predicted_label)})
+    return jsonify({'prediction': int(label)})
 
 if __name__ == "__main__":
     app.run(debug=True)
