d1054d0d4e1a23a31a119b202d1469606bdfefa7
Garv <garv1908@gmail.com>
2024-06-17 17:07:51 +0400

    clean up comments

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-# from flask import Flask, render_template, request, jsonify
-# from nn import GetPrediction
-# import numpy as np
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     data = request.get_json()
-#     imgArr = np.array(data['image']).reshape(784, 1)
-#     prediction = GetPrediction(imgArr)
-#     predictedLabel = np.argmax(prediction)
-#     print(prediction)
-#     return jsonify({'prediction': int(predictedLabel)})
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, jsonify
 from nn import GetPrediction
 from PIL import Image
@@ -36,38 +14,17 @@ def index():
 
 def processImage(imageData):
 
-    print("in processImage")
     imageData = imageData.split(',')[1]
-    print("imageData", imageData)
     imageBytes = base64.b64decode(imageData)
-    print("imageBytes", imageBytes)
-
     image = Image.open(io.BytesIO(imageBytes))
-    print("image open")
-    # image.show()
+
     image = image.resize((28, 28))
-    print("image", image)
     image = np.array(image)
 
-    print('shape:', image.shape[:])
-    # print(image[:, :, 0])
-    # print(image[:, :, 1])
-    # print(image[:, :, 2])
-    # print(image[:, :, 3])
     greyImage = image[:, :, 3]
-    # plt.imshow(image, cmap="Greys")
-    # plt.show()
-    print(image)
-    print(greyImage)
-    print('shape:', greyImage.shape[:])
-    # plt.imshow(greyImage, cmap="Greys")
-    # plt.show()
-    print("np arr greyImage", greyImage)
+
     greyImage = greyImage.astype("float32") / 255
-    print("greyImage as type normalized", greyImage)
-    greyImage = np.reshape(greyImage, (greyImage.shape[0] * greyImage.shape[1]))
-    print("greyImage as type normalized", greyImage)
-    print('shape after:', greyImage.shape[:])
+
     return greyImage
 
 @app.route("/predict", methods=["POST"])
