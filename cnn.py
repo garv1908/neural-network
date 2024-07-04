765a936e6a200984efe7f3d22a9e83ea43766cdb
Garv <garv1908@gmail.com>
2024-07-04 18:40:15 +0400

    complete cnn.py and make it usable

--- cnn.py
+++ cnn.py
@@ -1,6 +1,7 @@
 from data import get_mnist
 import numpy as np
 import tensorflow as tf
+import pickle
 
 print('Tensorflow version:', tf.__version__)
 print('Keras version:', tf.keras.__version__)
@@ -11,7 +12,7 @@ class ConvolutionalNN():
         self.images = np.reshape(self.images, (self.images.shape[0], self.IMAGE_WIDTH, self.IMAGE_HEIGHT, self.IMAGE_CHANNELS))
 
         
-    def train(self):
+    def train(self, epochs):
         self.model = tf.keras.models.Sequential()
 
         self.model.add(tf.keras.layers.Convolution2D(
@@ -62,21 +63,51 @@ class ConvolutionalNN():
 
         self.model.compile(
             optimizer = adam_optimizer,
-            loss = tf.keras.losses.sparse_categorical_crossentropy,
+            loss = tf.keras.losses.categorical_crossentropy,
             metrics = ['accuracy']
         )
 
+        training_history = self.model.fit(
+            self.images,
+            self.labels,
+            epochs=epochs
+        )
+
+        self.save_model()
+
 
     def get_prediction(self, img):
-        pass
+        img = np.expand_dims(img, axis=-1)
+        img = np.expand_dims(img, axis=0)
+        prediction = self.model.predict(img)
 
-
+        from icecream import ic
+        ic(prediction)
+        
+        return prediction[0]
+    
+    def load_model(self, filename="./tmp/loaded_models/convolutional.pkl"):
+        try:
+            with open(filename, "rb") as file:
+                self.model = pickle.load(file)
+        except:
+            print("Error loading the model. Retraining...")
+            open(filename, 'w').close()
+            self.train(epochs=1)
+            return self
+        
+    def save_model(self, filename="./tmp/loaded_models/convolutional.pkl"):
+        # model_filename = "./tmp/loaded_models/convolutional.pkl"
+        with open(filename, "wb") as file:
+            pickle.dump(self.model, file)
+        print("model saved to:", filename)
 
 
 if __name__ == "__main__":
     
     cnn = ConvolutionalNN()
-    cnn.train()
+    cnn.train(epochs=1)
+    
 
     # show the results
     # while True:
